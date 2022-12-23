a6f4fea6dbf23019f2d4291e2dd2df222f92c26f
ballabanidesara <104009829+ballabanidesara@users.noreply.github.com>
2022-12-23 12:34:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FakeEcommerce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-dataview-demo-szml3a)
---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { ProductService } from './productservice';
+import { Product } from './product';
+import { SelectItem } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public products: Observable<Product[]>;
+
+  sortOptions: SelectItem[];
+
+  sortOrder: number;
+
+  sortField: string;
+
+  constructor(
+    private productService: ProductService,
+    private primengConfig: PrimeNGConfig
+  ) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe((res) => {
+      this.products = res;
+    });
+
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!price' },
+      { label: 'Price Low to High', value: 'price' },
+    ];
+
+    this.primengConfig.ripple = true;
+  }
+
+  onSortChange(event) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
+}
